db6f9e1533a4016d1d17be2480b3b33602a31221
tlfolkman <44623133+tlfolkman@users.noreply.github.com>
2020-01-08 19:46:09 -0700

    Aggressor File

---
+++ Aggressor.java
@@ -0,0 +1,17 @@
+package folkman;
+
+/**
+*  Aggressors react to creatures they encounter.
+* <p>
+* @author  Brother Falin
+* @version 1.0
+* @since   2016-12-08 
+* @see Creature
+*/
+public interface Aggressor {
+
+	/**
+    * @param target The {@link Creature} we've encounterd.
+    */
+	public void attack(Creature target);
+}
