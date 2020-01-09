2bbbfbace3e671a92fe904fcdb1a10eeb75625d4
tlfolkman <44623133+tlfolkman@users.noreply.github.com>
2020-01-08 19:52:08 -0700

    Movable Interface

---
+++ Movable.java
@@ -0,0 +1,17 @@
+package folkman;
+
+/**
+*  Aggressors react to creatures they encounter.
+* <p>
+* @author  Brother Falin
+* @version 1.0
+* @since   2016-12-08 
+* @see Creature
+*/
+public interface Movable {
+
+	/**
+    * Moves the {@link Creature}.
+    */
+	public void move();
+}
